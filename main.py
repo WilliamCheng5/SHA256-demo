a10c505fa5e4cf4901675b8fa63dc20064d7136b
WilliamChengDev <williamcheng04930@gmail.com>
2026-07-30 18:13:57 -0500

    completed preprocessing and word splitting steps.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import transaction_generator
+import sha256
 
 T_COUNT = 500 # number of transactions
 
@@ -6,4 +7,6 @@ T_COUNT = 500 # number of transactions
 transactions = []
 for i in range(T_COUNT):
     transactions.append(transaction_generator.generate_transaction())
-print(transactions[:10])
+# print(transactions[:10])
+
+sha256.sha256(transactions[0])
--- sha256.py
+++ sha256.py
@@ -1,1 +1,31 @@
 #---------------------------Hash all transactions---------------------------#
+def sha256(b):
+        b = preprocess(b)
+
+        # split message into 512-bit blocks, with 16 32-bit words
+        blocks = []
+        for i in range (len(b)//64): 
+                words = []
+                for k in range(16): 
+                        words.append(b[64*i+4*k:64*i+4*(k+1)])
+                        print(f"word: {words[k]}")
+                blocks.append(words)
+        print(blocks)
+
+def preprocess(b):
+        og_len = len(b) * 8
+
+        # append 1 bit + seven 0 bits
+        b.append(0x80)
+        # append zeros until 64-bit length field fits
+        while len(b) % 64 != 56:
+                b.append(0x00)
+        # append original length as 64-bit big endian
+        b += og_len.to_bytes(8, "big")
+
+        for byte in b:
+                print(f"{byte:08b}", end="")
+        print()
+        print(f"padded size:{len(b)*8}")
+
+        return b
